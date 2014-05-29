a29c2fd386937b3d93ce1b788199f3e51e53926b
moznion <moznion@gmail.com>
2014-05-29 10:58:56 +0900

    Implement basic function

---
+++ index.js
@@ -0,0 +1,52 @@
+'use strict';
+
+var tsd     = require('./lib/tsd');
+var through = require('through2');
+var gutil   = require('gulp-util');
+
+module.exports = function () {
+    var settings = [];
+
+    var promised = function (context, promise, callback) {
+        promise.done(function () {
+            return callback();
+        }, function (err) {
+            self.emit('error', new gutil.PluginError('gulp-tsd', 'Failed command execution: ' + err.stack));
+            return callback();
+        });
+    }
+
+    function transform(file, encode, callback) {
+        if (file.isNull()) {
+            this.push(file);
+            return callback();
+        }
+
+        if (file.isStream()) {
+            this.emit('error', new gutil.PluginError('gulp-tsd', 'Streaming not supported'));
+            return callback();
+        }
+
+        var pathname = './' + file.relative.replace(/\\/g, '/');
+        settings.push(require(pathname));
+
+        callback();
+    }
+
+    function flush(callback) {
+        var self = this;
+        settings.forEach(function (setting) {
+            var command = setting.command;
+            var tsd_command = tsd.getRunner(gutil).commands[command];
+            if (typeof tsd_command === 'undefined') {
+                self.emit('error', new gutil.PluginError('gulp-tsd', '"' + command + '"' + ' command is not supported'));
+                return callback();
+            }
+
+            return promised(self, tsd_command(setting), callback);
+        });
+    }
+
+    return through.obj(transform, flush);
+}
+
