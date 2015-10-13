1b1a5217dd08353f3b1d9b546220d26304cca295
Sam Noedel <sam.noedel@changepoint.com>
2015-10-13 09:01:21 -0700

    Adding option to suppress build output

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ module.exports = function (options, callback) {
 
     var logger = {
         'log': function () {
+            if (options.quiet) {
+                return;
+            }
+
             var args = Array.prototype.slice.call(arguments);
             args.unshift('[' + gutil.colors.cyan('gulp-tsd') + ']');
             gutil.log.apply(undefined, args);
